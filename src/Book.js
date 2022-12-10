50be85203976c29a4ac7a93d888e10b0e4f4fabb
Ulysses Cortes <ulyssescortes13@gmail.com>
2022-12-09 21:37:03 -0600

    Ingredients implemented

--- src/Book.js
+++ src/Book.js
@@ -6,24 +6,14 @@ const Book = (props) => {
     const [isOpen, setIsOpen] = useState(false)
     const { img, title, ingredients } = props;
 
-
-    // Used this functions to test 
-    const ingredientsMap = () => {
-        ingredients.map((ingredient) => {
-            return (
-                <main>
-                    <p>{ingredient}</p>
-                    {console.log(ingredient)}
-                    {console.log(typeof ingredient)}
-                </main>
-            )
-        })
-    }
     const testParagraph = () => {
         return (
             <div >
-                <p>{ingredients}</p>
-                {/* <p>{ingredients.forEach((ingredient) => { <p>{ingredient}</p> })}</p> */}
+                <p>{ingredients.map((ingredient) => {
+                    return (
+                        <p> {"* " + ingredient}</p>
+                    )
+                })}</p>
             </div >
         )
 
@@ -34,16 +24,9 @@ const Book = (props) => {
             <h1>{title}</h1>
             <img src={img} alt="" />
             <button onClick={() => setIsOpen(!isOpen)} className="btn">Receta</button>
-            {console.log(props)}
             <div>
-                {isOpen && testParagraph()}
-                {/* I have test paregraph just for testing but the map is not */}
-                {/* {ingredientsMap()} */}
-
+                <p>{isOpen && testParagraph()}</p>
             </div>
-
-            {/* {isOpen && <div className="content"> {ingredientsForEach()}</div>} */}
-
         </article >
     )
 }
